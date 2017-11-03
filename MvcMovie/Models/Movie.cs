9d8ffc2baf090db900735e28365474378f14ea1e
david kiernan <davidkiernan@hotmail.co.uk>
2017-11-03 12:12:33 +0000

    Completed Tutorial 6 Examining edit methods & edit view

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 namespace MvcMovie.Models
 {
@@ -6,6 +7,10 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        //[DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)] // culture specific
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
